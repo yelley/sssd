692bbacf028112152c0cb6a8658cfb46c37af276
Yassir Elley <yelley@redhat.com>
2014-01-23 14:29:45 -0500

    Added ability to:
    * retrieve policy target DN based on sAMAccountName
    * parse out SOM DNs (i.e. OU DNs and DC DN) from policy target DN
    * retrieve gPLink attribute for SOM DN

--- src/providers/ad/ad_gpo.c
+++ src/providers/ad/ad_gpo.c
@@ -28,25 +28,122 @@
 #include "src/providers/dp_backend.h"
 #include "src/providers/ad/ad_access.h"
 #include "src/providers/ad/ad_common.h"
+#include "src/providers/ad/ad_gpo.h"
 #include "src/providers/ldap/sdap_access.h"
 #include "src/providers/ldap/sdap_async.h"
 #include "src/providers/ldap/sdap.h"
 
+struct ad_gpo_access_state {
+  struct tevent_context *ev;
+  struct ad_access_ctx *ctx;
+  struct pam_data *pd;
+  struct be_ctx *be_ctx;
+  struct sss_domain_info *domain;
+  struct sdap_id_conn_ctx *conn;
+  struct sdap_id_op *sdap_op; 
+  struct sdap_handle *sh;
+  struct sdap_options *opts;
+  char *basedn;
+  char **som_list;
+  int som_index;
+};
+
+
+#define AD_AT_DN "distinguishedName"
 #define AD_AT_GPLINK "gPLink"
 #define AD_AT_GPOPTIONS "gpOptions"
 
-struct ad_gpo_access_state {
-    struct tevent_context *ev;
-    struct ad_access_ctx *ctx;
-    struct pam_data *pd;
-    struct be_ctx *be_ctx;
-    struct sss_domain_info *domain;
-    struct sdap_id_conn_ctx *conn;
-    struct sdap_id_op *sdap_op; 
-};
+/* maybe ad_gpo_parent_dn should be in util.c?? */
+static char *ad_gpo_parent_dn(const char *dn)
+{
+  char *p;
 
-static int ad_gpo_access_step(struct tevent_req *req);
+  if (dn == NULL) {
+    return NULL;
+  }
+
+  p = strchr(dn, ',');
+
+  if (p == NULL) {
+    return NULL;
+  }
+
+  return p+1;
+}
+
+/* 
+ * The ad_gpo_parse_dn function implements the parsing logic specified in 3.2.5.1.3 of [MS-GPOL], which is the section on Domain Scope of Management (SOM) Search.
+ * This function essentially repeatedly adds the input DN's parent to the SOM List (if the parent starts with "OU=" or "DC="), until the first "DC=" component is reached.
+ * For example:
+ *   If the input DN is "CN=MyComputer,OU=Sales,DC=FOO,DC=COM",     then the SOM List consists of two SOM entries:    {[OU=Sales,DC=FOO,DC=COM], [DC=FOO, DC=COM]} 
+ *   If the input DN is "CN=MyComputer,CN=Computers,DC=FOO,DC=COM", then the SOM List consists of a single SOM entry: {[DC=FOO, DC=COM]} 
+ */
+static errno_t ad_gpo_parse_dn(TALLOC_CTX *mem_ctx, char *dn, const char *basedn, char ***_som_list, int *_som_count) {
+
+    TALLOC_CTX *tmp_ctx = NULL;
+    int rdn_count = 0;
+    int som_count = 0;
+    char **som_list;
+    char *parent_dn = NULL;
+    char *tmp_dn = NULL;
+
+    DEBUG(1, ("YKE\n"));
+
+    tmp_ctx = talloc_new(NULL);
+    
+    tmp_dn = dn;
+    while ((parent_dn = ad_gpo_parent_dn(tmp_dn))){
+      rdn_count++;
+      tmp_dn = parent_dn;
+    }
+
+    /* assume the worst-case, in which every parent is a SOM */
+    som_list = talloc_array(tmp_ctx, char *, rdn_count + 1);
+    if (!som_list) {
+      DEBUG(1, ("returning ENOMEM\n"));
+      return ENOMEM;
+    }
+
+    tmp_dn = dn;
+    while ((parent_dn = ad_gpo_parent_dn(tmp_dn))){
+
+      DEBUG(1, ("parent_dn is %s\n", parent_dn));
+      
+      if (strncasecmp(parent_dn, "OU=", strlen("OU=")) == 0) {
+	som_list[som_count++] = talloc_strdup(som_list, parent_dn);
+      } else if (strncasecmp(parent_dn, "DC=", strlen("DC=")) == 0) {
+	som_list[som_count++] = talloc_strdup(som_list, parent_dn);
+	break;
+      }
+      
+      tmp_dn = parent_dn;
+    }
+      
+    som_list[som_count] = NULL;
+
+    *_som_list = talloc_steal(mem_ctx, som_list);
+    *_som_count = som_count;
+
+    talloc_free(tmp_ctx);
+    return EOK;
+}
+
+/* parse gplink, which is in the following format:
+ *  [<GPO DN_1>;<GPLinkOptions_1>][<GPO DN_2>;<GPLinkOptions_2>]...[<GPODN_n>;<GPLinkOptions_n>]
+ */
+
+/*
+static errno_t parse_gplink(TALLOC_CTX *mem_ctx, char *gplink, struct GP_LINK *gp_link )
+{
+return 0;
+
+}
+
+*/
+
 static void ad_gpo_connect_done(struct tevent_req *subreq);
+static void ad_gpo_dn_retrieval_done(struct tevent_req *subreq);
+static errno_t ad_gpo_get_som_attrs_step(struct tevent_req *subreq, char *som_dn);
 static void ad_gpo_som_done(struct tevent_req *subreq);
 
 struct tevent_req *
@@ -58,10 +155,11 @@ ad_gpo_access_send(TALLOC_CTX *mem_ctx,
 		struct pam_data *pd)
 {
     struct tevent_req *req;
+    struct tevent_req *subreq;
     struct ad_gpo_access_state *state;
     errno_t ret;
 
-    DEBUG(1, ("Entering ad_gpo_access_send.\n"));
+    DEBUG(1, ("YKE\n"));
 
     req = tevent_req_create(mem_ctx, &state, struct ad_gpo_access_state);
     if (req == NULL) {
@@ -74,80 +172,54 @@ ad_gpo_access_send(TALLOC_CTX *mem_ctx,
     state->be_ctx = be_ctx;
     state->domain = domain;
     state->conn = ad_get_dom_ldap_conn(ctx->ad_id_ctx, domain);
-
-    DEBUG(1, ("Creating LDAP connection for GPO access checking\n"));
+    state->sh = NULL;
+    state->opts = state->ctx->sdap_access_ctx->id_ctx->opts;
 
     state->sdap_op = sdap_id_op_create(state, state->conn->conn_cache);
 
     if (!state->sdap_op) {
         DEBUG(2, ("sdap_id_op_create failed\n"));
         ret = ENOMEM;
-        goto done;
+        goto immediately;
     }
-
-    ret = ad_gpo_access_step(req);
-    if (ret != EOK) {
-        goto done;
-    }
-
-    ret = EOK;
-done:
-    if (ret != EOK) {
-        tevent_req_error(req, ret);
-        tevent_req_post(req, ev);
-    }
-    return req;
-
-}
-
-static int ad_gpo_access_step(struct tevent_req *req)
-{
-    struct tevent_req *subreq;
-    struct ad_gpo_access_state *state;
-    int ret;
-
-    state = tevent_req_data(req, struct ad_gpo_access_state);
 
     subreq = sdap_id_op_connect_send(state->sdap_op, state, &ret);
-    if (!subreq) {
-        DEBUG(2, ("sdap_id_op_connect_send failed: %d (%s)\n", ret, strerror(ret)));
-        return ret;
+    if (subreq == NULL) {
+        ret = ENOMEM;
+        goto immediately;
     }
 
     tevent_req_set_callback(subreq, ad_gpo_connect_done, req);
-    return EOK;
+
+    return req;
+
+ immediately:
+
+    tevent_req_error(req, ret);
+    tevent_req_post(req, ev);
+
+    return req;
+
 }
 
 static void
 ad_gpo_connect_done(struct tevent_req *subreq)
 {
-    struct tevent_req *req;
+  struct tevent_req *req;
     struct ad_gpo_access_state *state;
-    char *basedn;
     char* filter;
+    char *ad_hostname;
+    char *sam_account_name;
     int dp_error;
     errno_t ret;
 
-    /* const char *attrs[] = {AD_AT_GPLINK, NULL};*/
-    const char *attrs[] = {
-            "*",
-            "altServer",
-            SDAP_ROOTDSE_ATTR_NAMING_CONTEXTS,
-            "supportedControl",
-            "supportedExtension",
-            "supportedFeatures",
-            "supportedLDAPVersion",
-            "supportedSASLMechanisms",
-            SDAP_ROOTDSE_ATTR_AD_VERSION,
-            SDAP_ROOTDSE_ATTR_DEFAULT_NAMING_CONTEXT,
-            SDAP_IPA_LAST_USN, SDAP_AD_LAST_USN,
-            NULL
-    };
+
+    DEBUG(1, ("YKE!!!\n"));
+
+    const char *attrs[] = {AD_AT_DN, NULL};
 
     req = tevent_req_callback_data(subreq, struct tevent_req);
     state = tevent_req_data(req, struct ad_gpo_access_state);
-
-    /*    sdap_options *opts = state->ctx->sdap_access_ctx->id_ctx->opts;*/
 
     ret = sdap_id_op_connect_recv(subreq, &dp_error);
     talloc_zfree(subreq);
@@ -167,54 +239,181 @@ ad_gpo_connect_done(struct tevent_req *subreq)
         return;
     }
 
-    /* Convert the domain name into domain DN */
-    ret = domain_to_basedn(state, state->domain->name, &basedn);
-    if (ret != EOK) {
-        DEBUG(SSSDBG_OP_FAILURE,
-            ("Cannot convert domain name [%s] to base DN [%d]: %s\n",
-            state->domain->name, ret, strerror(ret)));
-        goto done;
+    ad_hostname = dp_opt_get_string(state->ctx->ad_options, AD_HOSTNAME);
+    if (!ad_hostname) {
+      /* Should be impossible, this is set in ad_get_common_options() */
+      DEBUG(1, ("no ad_hostname\n"));
+      tevent_req_error(req, EINVAL);
+      return;
     }
 
-    DEBUG(1, ("domain_to_basedn yields %s\n", basedn));
+    sam_account_name = talloc_asprintf(state, "%s$", ad_hostname);
+    if (sam_account_name == NULL) {
+        DEBUG(1, ("talloc_asprintf() failed\n"));
+	tevent_req_error(req, ENOMEM);
+	return;
+    }
+    DEBUG(1, ("sam_account_name is %s\n", sam_account_name));
 
-    filter = talloc_asprintf(state, "(distinguishedName=%s)", basedn);
+    /* Convert the domain name into domain DN */
+    ret = domain_to_basedn(state, state->domain->name, &state->basedn);
+    if (ret != EOK) {
+      DEBUG(SSSDBG_OP_FAILURE,
+            ("Cannot convert domain name [%s] to base DN [%d]: %s\n",
+	     state->domain->name, ret, strerror(ret)));
+      tevent_req_error(req, ret);
+      return;
+    }
+
+    /* "computer" objectclass inherits from "user" objectclass */
+    filter = talloc_asprintf(state, "(&(objectclass=user)(sAMAccountName=%s))", sam_account_name);
     if (filter == NULL) {
         ret = ENOMEM;
-        goto done;
+        tevent_req_error(req, ret);
+        return;
     }
 
-    DEBUG(1, ("filter is %s\n", filter));
-    DEBUG(1, ("ad_gpo_connect_done about to call sdap_get_generic_send\n"));
-    subreq = sdap_get_generic_send(state,
-				   state->ev, 
-				   state->ctx->sdap_access_ctx->id_ctx->opts,
-                                   sdap_id_op_handle(state->sdap_op),
-                                   "", LDAP_SCOPE_BASE,
-                                   "(objectclass=*)", attrs, NULL, 0,
-                                   dp_opt_get_int(state->ctx->sdap_access_ctx->id_ctx->opts->basic,
-                                                  SDAP_SEARCH_TIMEOUT),
+    int timeout = dp_opt_get_int(state->opts->basic, SDAP_SEARCH_TIMEOUT);
+    state->sh = sdap_id_op_handle(state->sdap_op);
+
+    DEBUG(1, ("basedn: %s\n", state->basedn));
+    DEBUG(1, ("filter: %s\n", filter));
+
+    subreq = sdap_get_generic_send(state, state->ev, state->opts, state->sh,
+				   state->basedn, LDAP_SCOPE_SUBTREE,
+				   filter, attrs, NULL, 0,
+                                   timeout,
 				   false);
 
-    DEBUG(1, ("YKE: completed sdap_get_generic_send\n"));
     if (subreq == NULL) {
         DEBUG(1, ("Could not start LDAP communication\n"));
         tevent_req_error(req, EIO);
         return;
     }
 
-    tevent_req_set_callback(subreq, ad_gpo_som_done, req);
+    tevent_req_set_callback(subreq, ad_gpo_dn_retrieval_done, req);
 
+    return;
+}
 
+static void
+ad_gpo_dn_retrieval_done(struct tevent_req *subreq)
+{
+    struct tevent_req *req;
+    struct ad_gpo_access_state *state;
+    int ret;
+    /*int dp_error;*/
+    size_t num_results;
+    struct sysdb_attrs **results;
+    struct ldb_message_element *el = NULL;
+    /*char* filter;*/
+    int som_count = 0;
+
+    DEBUG(1, ("YKE!\n"));
+
+    req = tevent_req_callback_data(subreq, struct tevent_req);
+    state = tevent_req_data(req, struct ad_gpo_access_state);
+    ret = sdap_get_generic_recv(subreq, state,
+                                &num_results, &results);
+    talloc_zfree(subreq);
+    if (ret != EOK) {
+        DEBUG(1, ("Unable to get policy target's DN\n"));
+	ret = ENOENT;
+	goto done;
+    }
+
+    DEBUG(1, ("num_results: %zu\n", num_results));
+
+    if (num_results < 1) {
+      DEBUG(1, ("No DN retrieved for policy target.\n"));
+      ret = ENOENT;
+      goto done;
+    } 
+    else if (results == NULL) {
+      DEBUG(1, ("num_results > 0, but results is NULL\n"));
+      ret = ERR_INTERNAL;
+      goto done;
+    } 
+    else if (num_results > 1) {
+        DEBUG(1, ("Received multiple replies\n"));
+        ret = ERR_INTERNAL;
+        goto done;      
+    }
+
+    /* if we reach here, we got exactly one result */    
+
+    ret = sysdb_attrs_get_el(results[0], AD_AT_DN, &el);
+    if (ret != EOK) {
+        DEBUG(1, ("sysdb_attrs_get_el() failed\n"));
+	goto done;
+    }
+
+    if (el->num_values == 0) {
+        DEBUG(1, ("DN has no value\n"));
+        ret = ENOENT;
+	goto done;
+    }  else if (el->num_values > 1) {
+        DEBUG(1, ("More than one DN value found?\n"));
+        ret = EIO;
+        goto done;
+    }
+
+    /* parse potentially multiple results */
+    char *computer_dn = (char *)el[0].values[0].data;
+    DEBUG(1, ("computer_dn is %s\n", computer_dn));
+
+    ret = ad_gpo_parse_dn(state, computer_dn, state->basedn, &state->som_list, &som_count);
+    if (ret != EOK) {
+      DEBUG(1, ("Unable to retrieve SOM List [%d]: %s\n", ret, strerror(ret)));
+        ret = ENOENT;
+        goto done;
+    }
+
+    for (int i = 0; i<som_count; i++){
+      DEBUG(1, ("som_list[%d]: %s\n", i, state->som_list[i]));
+    }
+
+    ret = ad_gpo_get_som_attrs_step(req, state->som_list[state->som_index]);
+    DEBUG(1, ("returned from step, ret is %d\n", ret));
+    if (ret == EOK) {
+      return;
+    }
 
  done:
-    if (ret == EOK) {
-      tevent_req_done(req);
-    } else {
-      tevent_req_error(req, ret);
-    }
-    return;
 
+    tevent_req_error(req, ret);
+    return;
+}
+
+static errno_t
+ad_gpo_get_som_attrs_step(struct tevent_req *req, char *som_dn)
+{
+
+    const char *attrs[] = {AD_AT_GPLINK, NULL};
+    struct tevent_req *subreq;
+    struct ad_gpo_access_state *state;
+
+    DEBUG(1, ("YKE\n"));
+
+    state = tevent_req_data(req, struct ad_gpo_access_state);
+
+    DEBUG(1, ("som_dn: %s\n", som_dn));
+
+    int timeout = dp_opt_get_int(state->opts->basic, SDAP_SEARCH_TIMEOUT);
+
+    subreq = sdap_get_generic_send(state, state->ev, state->opts, state->sh,
+				   som_dn, LDAP_SCOPE_BASE,
+				   "(objectclass=*)", attrs, NULL, 0,
+                                   timeout,
+				   false);
+
+    if (subreq == NULL) {
+        DEBUG(1, ("Could not start LDAP communication\n"));
+	return ENOMEM;
+    }
+
+    tevent_req_set_callback(subreq, ad_gpo_som_done, req);
+    return EOK;
 }
 
 static void
@@ -225,32 +424,79 @@ ad_gpo_som_done(struct tevent_req *subreq)
     int ret, dp_error;
     size_t num_results;
     struct sysdb_attrs **results;
+    struct ldb_message_element *el = NULL;
+    uint8_t *gplink;
+    size_t length;
+    /*    struct GP_LINK *gp_link;*/
 
+    DEBUG(1, ("YKE\n"));
     req = tevent_req_callback_data(subreq, struct tevent_req);
     state = tevent_req_data(req, struct ad_gpo_access_state);
-
-    DEBUG(1, ("ad_gpo_som_done about to call sdap_get_generic_recv\n"));
     ret = sdap_get_generic_recv(subreq, state,
                                 &num_results, &results);
     talloc_zfree(subreq);
-    if (ret) {
+
+    ret = sdap_id_op_done(state->sdap_op, ret, &dp_error);
+
+    if (ret != EOK) {
+        DEBUG(1, ("ret != EOK\n"));
+        if (dp_error == DP_ERR_OK) {
+	  DEBUG(1, ("dp_error is DP_ERR_OK"));
+            /* retry */
+        } else if (dp_error == DP_ERR_OFFLINE) {
+	  DEBUG(1, ("dp_error is DP_ERR_OFFLINE"));
+	  /* ret = ad_gpo_access_decide_offline(req); */
+        } else {
+            DEBUG(1, ("sdap_get_generic_send() returned error [%d][%s]\n",
+                      ret, strerror(ret)));
+        }
+
+        goto done;
+    }
+
+
+    /* we could get zero, one, or multiple results */
+    if (num_results < 1) {
+      DEBUG(1, ("no gplinks found. Denying access.\n"));
+      ret = ERR_INTERNAL;
       goto done;
     }
-
-    DEBUG(1, ("num_results=%d\n, num_results"));
-
-    if (num_results == 0){
-      DEBUG(1, ("num_result is 0\n"));
-      tevent_req_error(req, ENOENT);
-      return;
+    else if (results == NULL) {
+        DEBUG(1, ("num_results > 0, but results is NULL\n"));
+        ret = ERR_INTERNAL;
+        goto done;
     }
 
-    if (num_results == 1){
-      DEBUG(1, ("num_result is 1\n"));
+
+    DEBUG(1, ("gplinks found!\n"));
+    DEBUG(1, ("num_results: %zu\n", num_results));
+
+    ret = sysdb_attrs_get_el(results[0], AD_AT_GPLINK, &el);
+    if (ret != EOK) {
+        DEBUG(1, ("sysdb_attrs_get_el() failed\n"));
+        goto done;
     }
 
-    DEBUG(1, ("YKE: about to call sdap_id_op_done\n"));
-    ret = sdap_id_op_done(state->sdap_op, ret, &dp_error);
+    if (el->num_values == 0) {
+        DEBUG(1, ("gplink has no value\n"));
+        ret = ENOENT;
+        goto done;
+    } 
+
+    /* parse potentially multiple results */
+
+    gplink = el[0].values[0].data;
+    length = el[0].values[0].length;
+
+    DEBUG(1, ("gplink is %s\n", gplink));
+    DEBUG(1, ("length is %zu\n", length));
+
+    if (ret != EOK) {
+        DEBUG(1, ("sysdb_attrs_get_el() failed\n"));
+        goto done;
+    }
+
+    /*    ret = parse_gplink(state, gplink, gp_link );*/
 
 done:
     if (ret == EOK) {
@@ -264,6 +510,7 @@ done:
 
 errno_t ad_gpo_access_recv(struct tevent_req *req)
 {
+    DEBUG(1, ("YKE\n"));
     TEVENT_REQ_RETURN_ON_ERROR(req);
 
     return EOK;
--- src/providers/ad/ad_gpo.h
+++ src/providers/ad/ad_gpo.h
@@ -33,4 +33,48 @@ ad_gpo_access_send(TALLOC_CTX *mem_ctx,
 
 errno_t ad_gpo_access_recv(struct tevent_req *req);
 
+
+/* following types are copied from git/samba/libgpo/gpo.h */
+enum GPO_LINK_TYPE {
+	GP_LINK_UNKOWN	= 0,
+	GP_LINK_MACHINE	= 1,
+	GP_LINK_SITE	= 2,
+	GP_LINK_DOMAIN	= 3,
+	GP_LINK_OU	= 4,
+	GP_LINK_LOCAL	= 5 /* for convenience */
+};
+
+struct GROUP_POLICY_OBJECT {
+	uint32_t options;	/* GPFLAGS_* */
+	uint32_t version;
+	const char *ds_path;
+	const char *file_sys_path;
+	const char *display_name;
+	const char *name;
+	const char *link;
+	enum GPO_LINK_TYPE link_type;
+	const char *user_extensions;
+	const char *machine_extensions;
+  /*struct security_descriptor *security_descriptor;*/
+	struct GROUP_POLICY_OBJECT *next, *prev;
+};
+
+struct GP_LINK {
+	const char *gp_link;	/* raw link name */
+	uint32_t gp_opts;	/* inheritance options GPO_INHERIT */
+	uint32_t num_links;	/* number of links */
+	char **link_names;	/* array of parsed link names */
+	uint32_t *link_opts;	/* array of parsed link opts GPO_LINK_OPT_* */
+};
+
+struct GP_EXT {
+	const char *gp_extension;	/* raw extension name */
+	uint32_t num_exts;
+	char **extensions;
+	char **extensions_guid;
+	char **snapins;
+	char **snapins_guid;
+	struct GP_EXT *next, *prev;
+};
+
 #endif /* AD_GPO_H_ */
